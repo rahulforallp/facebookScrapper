d86f84a301c01bf474297560380cc065204bd636
Rahul <rahulforallp@gmail.com>
2016-05-12 10:29:30 +0530

    movies list extracted

--- src/main/java/fb.java
+++ src/main/java/fb.java
@@ -16,22 +16,22 @@ public class fb {
 
         Facebook facebook = new FacebookFactory().getInstance();
         try {
-            System.out.println("oauth start");
+
             facebook.setOAuthAppId("","");
-            String accessTokenString = "EAACEdEose0cBAMG7tBAhpc6QiiZAOvTsaUZBrGZBo1COohOmvvGe1bvQadw0weq0NE4XSS3wbo2lZBcLRjZBzP1ZB13qopnU9Q1IEExLrrx1WRqWzYUldYOzZBfOMZBMB12GUxCmp8MZCBrIzEsBNkvDGbqnzN3WSaZB2MGOKpp8lZB7oFMoODecE1ZC";
+            String accessTokenString = "EAACEdEose0cBANWgxtNTJonarayJyB7WTORJ6NxdZBDQDgZCBgCAezaZBmN0aEOPk08tWsZCIdBj7A6YCaFt1pzyxHHpp1veBIBE6ZBnDB6EzN5dowFSZAwyYR14OcW7USXp416afNZCnkUjrFEnffOiiPZBTAUURtc1KyBC7iSecu5Rpmh70cKx";
             AccessToken at = new AccessToken(accessTokenString);
             facebook.setOAuthAccessToken(at);
-            System.out.println("oauth start");
+
 
            // facebook.postStatusMessage("Hello World from Facebook4J.");
 
-            String query = "SELECT email,birthday FROM user WHERE uid = me()";
+            String query = "SELECT movies,email from user where uid=me()";
 
             JSONArray jsonArray = facebook.executeFQL(query);
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                System.out.print(jsonObject.get("email")+",");
-                System.out.println(jsonObject.get("birthday"));
+                System.out.println("My email : "+jsonObject.get("email"));
+                System.out.println(jsonObject.get("movies")+",");
             }
 
         } catch (Exception e) {
